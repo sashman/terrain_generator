5b784c3674c8fa79acce0b7bc0833ae21bd680a2
Sash Man <sashman90@gmail.com>
2012-01-23 02:07:24 +0000

    readme script now appends todo into readme file

--- scripts/readme_todo.sh
+++ scripts/readme_todo.sh
@@ -3,6 +3,9 @@
 ./../src/a.out -h > ../README
 
 echo TODO: > ../TODO
+echo "" >> ../README
+echo "" >> ../README
+echo "TODO:" >> ../README
 
 while read line
 do 
@@ -11,5 +14,6 @@ l=(`echo $line | tr ' ' ' '`)
 if [ "${l[0]}" == '//TODO:' ]
 	then
 	echo "-" ${l[@]:1} >> ../TODO
+	echo "-" ${l[@]:1} >> ../README
 fi
 done < "../src/terrain_generator.cpp"
