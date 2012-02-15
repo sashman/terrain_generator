118015a779a55e8c0b2d18097d9cd9df155fe8d8
Sash Man <sashman90@gmail.com>
2012-02-15 18:08:27 +0000

    added settlement generation, can be viewed using the view_colour_settlements script

---
+++ scripts/view_colour_settlements.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+
+import sys
+
+
+libs_needed = []
+try:
+	import PIL
+except Exception:
+	libs_needed.append("python-imaging")
+
+try:
+	import lxml
+except Exception:
+	libs_needed.append("python-lxml")
+
+if(len(libs_needed)>0):
+	print "Required libs: "
+	print ",".join(libs_needed)
+	exit()
+
+from PIL import Image, ImageDraw, ImageFont
+from lxml import etree
+
+class Settlement():
+	def __init__(self, x, y, name):
+		self.x = x
+		self.y = y
+		self.name = name
+
+#terrain data
+map_vals = open(sys.argv[1], 'r').read().split()
+w = int(map_vals[0])
+h = int(map_vals[1])
+max_val = max([int(x) for i,x in enumerate(map_vals) if i > 1])
+
+
+#settelement data
+settlements_string = open(sys.argv[2], 'r').read()
+
+root = etree.fromstring(settlements_string)
+set_elems = root.findall("settlement")
+settlements = []
+for se in set_elems:
+	s = Settlement(se.get("y"), se.get("x"), se.findtext("name"))
+	settlements.append(s)
+
+
+#draw image
+img = Image.new("RGB", (w,h), (256,256,256))
+xsize,ysize = w,h
+draw = ImageDraw.Draw(img)
+
+for i in range(0,h):
+	for j in range(2,w):
+		v = int(map_vals[i*h + j])
+		v = int(float(v)/float(max_val) * 255)
+		
+		if(v<150):
+			draw.point((i,j), fill=(0,0,100+v))
+		elif(v<160):
+			draw.point((i,j), fill=(v,v,0))
+		elif(v<210):
+			draw.point((i,j), fill=(0,200-(v/2),0))
+		else:
+			draw.point((i,j), fill=(v-60,v-10,v-60))
+
+# use a truetype font
+font = ImageFont.truetype("VeniceClassic.ttf", 18)
+
+for s in settlements:
+	v = int(map_vals[int(s.x)*h + int(s.y)])
+	v = int(float(v)/float(max_val) * 255)
+
+	draw.rectangle((int(s.x)-2, int(s.y)-2, int(s.x)+2, int(s.y)+2), fill=(0))
+
+	x_pos = int(s.x) - 10
+	y_pos = int(s.y) + 5
+
+	if(int(s.y) > 500):
+		x_pos = int(s.x)+6
+		y_pos = int(s.y)-10
+	if(int(s.x) > 420):
+		x_pos = int(s.x)-25	
+
+	draw.text((x_pos, y_pos), s.name, fill=(0), font=font)
+		
+del draw
+img.save("out.png")
+img.show()
+
+
--- src/settlements.cpp
+++ src/settlements.cpp
@@ -10,30 +10,77 @@
 #include <locale>
 
 int n_settlements = DEFAULT_NO_OF_SETTLEMENTS;
+int min_distance = DEFAULT_MIN_DISTANCE_BETWEEN_SETTLEMENTS;
 
 extern int crop_height;
 extern int crop_width;
 extern int** tmap;
 
+bool* settlement_found;
 int** locations;
 std::string* names;
+
+int sea_level = DEFAULT_SEA_LEVEL;
+int snowtop_level = DEFAULT_SNOW_TOP_LEVEL;
 
 /*
  * attempts to find a location above sea level
  * if so sets as the location and returns true
  * else false
  */
-bool above_sea_level(int location_index) {
+bool get_above_sea_level(int location_index) {
 
 	//create a settlement above sea level at a random location
 	int x = rand() % crop_width;
 	int y = rand() % crop_height;
 
 	//TODO: change to a set sea level!
-	int sea_level = 0.588 * 126;
+	sea_level = 0.588 * 126;
 
+	while (tmap[y][x] <= sea_level) {
+		x = rand() % crop_width;
+		y = rand() % crop_height;
+	}
 
-	while (tmap[x][y] <= sea_level) {
+	locations[location_index][0] = x;
+	locations[location_index][1] = y;
+
+	return true;
+}
+
+bool point_above_sealevel(int x, int y) {
+	return tmap[y][x] > sea_level;
+}
+
+bool point_below_snow_top_level(int x, int y) {
+	return tmap[y][x] < snowtop_level;
+}
+
+bool point_at_min_distance(int x, int y, int index) {
+
+	for (int i = 0; i < index; ++i) {
+		int d = sqrt(
+				abs((pow(x, 2) - pow(locations[i][0], 2)))
+						+ abs((pow(y, 2) - pow(locations[i][1], 2))));
+
+		if (d < min_distance)
+			return false;
+	}
+	return true;
+}
+
+/*
+ * create settlement wrt given constraints
+ *
+ */
+bool get_settelement(int location_index) {
+
+	//start with random
+	int x = rand() % crop_width;
+	int y = rand() % crop_height;
+
+	while (!point_above_sealevel(x, y) || !point_below_snow_top_level(x, y)
+			|| !point_at_min_distance(x, y, location_index)) {
 		x = rand() % crop_width;
 		y = rand() % crop_height;
 	}
@@ -45,7 +92,7 @@ bool above_sea_level(int location_index) {
 
 }
 
-void get_name(std::string *name){
+void get_name(std::string *name) {
 
 	FILE* fprefix = fopen("location_prefix", "r");
 	FILE* fsuffix = fopen("location_suffix", "r");
@@ -55,19 +102,21 @@ void get_name(std::string *name){
 	else {
 
 		int fprefix_count = 0;
-		fscanf(fprefix,"%d\n", &fprefix_count);
+		fscanf(fprefix, "%d\n", &fprefix_count);
 
 		int fsuffix_count = 0;
-		fscanf(fsuffix,"%d\n", &fsuffix_count);
+		fscanf(fsuffix, "%d\n", &fsuffix_count);
 
-		int prefix_i = rand()%fprefix_count+1;
-		int suffix_i = rand()%fsuffix_count+1;
+		int prefix_i = rand() % fprefix_count + 1;
+		int suffix_i = rand() % fsuffix_count + 1;
 
 		char prefix[128];
 		char suffix[128];
 
-		for (int i = 0; i < prefix_i; ++i) fscanf(fprefix, "%s\n", &prefix);
-		for (int i = 0; i < suffix_i; ++i) fscanf(fsuffix, "%s\n", &suffix);
+		for (int i = 0; i < prefix_i; ++i)
+			fscanf(fprefix, "%s\n", &prefix);
+		for (int i = 0; i < suffix_i; ++i)
+			fscanf(fsuffix, "%s\n", &suffix);
 
 		*name += prefix;
 		*name += suffix;
@@ -78,26 +127,25 @@ void get_name(std::string *name){
 	fclose(fprefix);
 	fclose(fsuffix);
 
-
 }
 
 void settlements() {
 
+	settlement_found = new bool[n_settlements];
 	locations = new int*[n_settlements];
 	names = new std::string[n_settlements];
 
 	for (int i = 0; i < n_settlements; ++i) {
 		locations[i] = new int[2];
 		names[i] = "";
-		//names[i] = new std::string;
+		settlement_found[i] = false;
 
 		//use a simple rule to find a suitable place for a settlement
-		bool found = above_sea_level(i);
+		while (!get_settelement(i));
 
 		//TODO: change to loop if not found
-		if (found)
-			get_name(&names[i]);
-
+		get_name(&names[i]);
+		settlement_found[i] = true;
 	}
 }
 
@@ -109,20 +157,21 @@ void print_settlements(FILE* stream) {
 		perror("Error opening file");
 	else {
 
-
 		fprintf(stream, "<settlements>\n");
 		for (int i = 0; i < n_settlements; ++i) {
-			fprintf(stream, "<settlement x='%d' y='%d'>\n", locations[i][0],
-					locations[i][1]);
+			if (settlement_found[i]) {
 
-			char* name = (char*)names[i].c_str();
-			name[0] = toupper(name[0]);
+				fprintf(stream, "<settlement x='%d' y='%d'>\n", locations[i][0],
+						locations[i][1]);
 
-			fprintf(stream, "<name>%s</name>\n", name);
-			fprintf(stream, "</settlement>\n");
+				char* name = (char*) names[i].c_str();
+				name[0] = toupper(name[0]);
+
+				fprintf(stream, "<name>%s</name>\n", name);
+				fprintf(stream, "</settlement>\n");
+			}
 		}
-		fprintf(stream, "<settlements>\n");
-
+		fprintf(stream, "</settlements>\n");
 
 	}
 }
